078e71eb344b306127e617e1bc856050c54e199e
Nuta Horia Catalin <45384660+hdott@users.noreply.github.com>
2018-12-05 03:34:17 +0200

    Create Ex2_6.c

---
+++ Fundamentals of Programming/1st Semester/Lab 2/Ex2_6.c
@@ -0,0 +1,15 @@
+/*
+Să se afişeze următorul text pe ecran (fără a utiliza caracterul spaţiu):
+'BB'        'BB'        "BB"        "BB"
+
+Terminarea programului se va realiza la apăsarea unei taste
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    printf("\t\'BB\'\t\'BB\'\t\"BB\"\t\"BB\"\n");
+
+    getchar();
+}
