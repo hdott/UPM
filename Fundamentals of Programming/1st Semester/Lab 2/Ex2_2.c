251df3ecb579b7a117b125e399b9dd929843cf66
Nuta Horia Catalin <45384660+hdott@users.noreply.github.com>
2018-12-05 02:46:51 +0200

    Create Ex2_2.c
    Să se afişeze următorul text pe ecran: 
    AAAAAAAAAAA 
    A        BB         A 
    AAAAAAAAAAA
    
    Terminarea programului se va realiza la apăsarea unei taste 
    Se vor încerca două variante: 
    - "ascunderea" tastei apăsate 
    - Afişarea tastei apăsate

---
+++ Fundamentals of Programming/1st Semester/Lab 2/Ex2_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("AAAAAAAAAAA\n");
+    printf("A    BB   A\n");
+    printf("AAAAAAAAAAA\n");    
+
+    //getch()   > ascunderea tastei apasate
+    //getche()  > afisarea tastei apasate
+}
