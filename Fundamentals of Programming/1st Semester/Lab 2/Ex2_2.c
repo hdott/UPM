37c793cd80c44b1130b40650f8ac3f05ea2f2daf
Nuta Horia Catalin <45384660+hdott@users.noreply.github.com>
2018-12-05 02:52:28 +0200

    Update Ex2_2.c

--- Fundamentals of Programming/1st Semester/Lab 2/Ex2_2.c
+++ Fundamentals of Programming/1st Semester/Lab 2/Ex2_2.c
@@ -1,3 +1,14 @@
+/*
+Să se afişeze următorul text pe ecran:
+AAAAAAAAAAA
+A    BB   A
+AAAAAAAAAAA
+
+Terminarea programului se va realiza la apăsarea unei taste:
+    - "ascunderea" tastei apăsate
+    - afişarea tastei apăsate
+*/
+
 #include <stdio.h>
 
 void main()
