5f892044158ff6ecd98c49bb638cf77ef4536a23
Nuta Horia Catalin <45384660+hdott@users.noreply.github.com>
2018-12-07 21:07:46 +0200

    Update Ex5_16.c

--- Fundamentals of Programming/1st Semester/Lab 5/Ex5_16.c
+++ Fundamentals of Programming/1st Semester/Lab 5/Ex5_16.c
@@ -4,7 +4,7 @@ Să se convertească un număr dat din baza 10 în baza 2, folosind algoritmul r
 
 #include <stdio.h>
 
-int print_baza2(int x)
+int baza10_to_baza2(int x)
 {
     int rezultat = 0;
     for(int i = 1; x/2 != 0 || x%2 != 0; i *= 10)
@@ -23,5 +23,5 @@ void main()
     printf("Introduceti un numar natural pentru a fi transformat in baza.2:\n");
         printf("nr = "), scanf("%d", &nr);
 
-    printf("\nNumarul %d in baza.2 (de la dreapta la stanga) este: %d\n", nr, print_baza2(nr));
+    printf("\nNumarul %d in baza.2 (de la dreapta la stanga) este: %d\n", nr, baza10_to_baza2(nr));
 }
