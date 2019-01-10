ce3560b2b31dadab8de4f6c17c6cfbc7672225f5
Nuta Horia Catalin <45384660+hdott@users.noreply.github.com>
2019-01-10 20:12:56 +0200

    Update P1.c

--- Fundamental Algorithms/C 6/P1.c
+++ Fundamental Algorithms/C 6/P1.c
@@ -3,6 +3,16 @@ Se considera functia f definita recursiv.
 Ce va returna f(5)?
 */
 
+/*
+OUTPUT:
+[5] -5
+[4] -1
+[3] -4
+[2] -2
+[1] -3
+[0] return to main
+*/
+
 #include <stdio.h>
 int debug=0;
 
