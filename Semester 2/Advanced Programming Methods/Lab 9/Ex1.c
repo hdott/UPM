0a31821c6dd8e65b09b48064947d91204a9ab25d
hdott <n.horia.c@hotmail.com>
2019-04-21 23:42:50 +0300

    ex1

---
+++ Semester 2/Advanced Programming Methods/Lab 9/Ex1.c
@@ -0,0 +1,49 @@
+// Heap's Algorithm
+
+#include <stdio.h>
+
+
+void swap(int *x, int *y)
+{
+    int aux = *x;
+    *x = *y;
+    *y = aux;
+}
+
+void printVector(int *vector, int size)
+{
+    for(int i=0; i<size; ++i)
+        printf("%d ", vector[i]);
+    puts("");
+}
+
+void heap(int *vector, int n, int size)
+{
+    if(n==1)
+    {
+        printVector(vector, size);
+        return;
+    }
+    
+    for(int i=0; i<n-1; ++i)
+    {
+        heap(vector, n-1, size);
+
+        if(!(n%2))  swap(&vector[n-1], &vector[i]);
+        else        swap(&vector[n-1], &vector[0]);
+    }
+
+    heap(vector, n-1, size);
+}
+
+
+int main(void)
+{
+    int vector[] = {1, 2, 3, 4, 5};
+    int size = sizeof(vector)/sizeof(vector[0]);
+
+    heap(vector, size, size);
+
+    return 0;
+}
+
