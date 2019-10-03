b24f15500229f154154bdb3ef77989b4e61f2103
hdott <n.horia.c@hotmail.com>
2019-10-03 08:57:13 +0300

    updated

--- Year - 2/Semester 1/Operating Systems/Lab 1/Ex1.c
+++ Year - 2/Semester 1/Operating Systems/Lab 1/Ex1.c
@@ -2,7 +2,10 @@
 
 
 int main (int argc, char **argv) {
-    for (int i=1; i<argc; ++i) {
-        printf("%s\n", *(argv+i));
-    }
+    if (argc > 1) {
+        for (int i=1; i<argc; ++i) {
+            printf("%s\n", *(argv+i));
+    } 
+
+    return 0;
 }
