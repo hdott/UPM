e2089aada07ce16c92e693ca1cd56b42491464c4
hdott <n.horia.c@hotmail.com>
2019-10-29 11:39:50 +0200

    in progress lab 5

---
+++ Year - 2/Semester 1/Visual Programming/Lab 5/ClassLibrary1/SQLiteHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Text;
+
+namespace DatabaseHandler
+{
+    public sealed class SQLiteHandler
+    {
+        private static SQLiteHandler instance = null;
+        private static readonly object padlock = new object();
+        private SQLiteConnection dbConexion = null;
+   
+        public static SQLiteHandler Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if(instance == null)
+                    {
+                        instance = new SQLiteHandler();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public bool ConnectToDb()
+        {
+            if(dbConexion == null)
+            {
+                if (!File.Exists("mydb.sqlite"))
+                {
+                    SQLiteConnection.CreateFile("mydb.sqlite");
+                }
+
+                dbConexion = new SQLiteConnection("DataSource=mydb.sqliteVersion=3;");
+                dbConexion.Open();
+                return true;
+            }
+
+            return false;
+        }
+
+        public void DisconnectFromDb()
+        {
+            if(dbConexion != null)
+            {
+
+            }
+        }
+    }
+}
