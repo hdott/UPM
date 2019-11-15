58fe07dc1f4c3a8d52ad8261d1188164b9ca56e0
hdott <n.horia.c@hotmail.com>
2019-11-15 18:56:38 +0200

    wip -l7 -f1

---
+++ Year - 2/Semester 1/Advanced Programming Techniques/Lab 7/Ex1/Ex_1.cpp
@@ -0,0 +1,39 @@
+/*
+    Sa se scrie functii sablon care pot prelua un vector unidimensional de orice tip si
+        calculeaza si returneaza:
+    - maximul din vector
+    - minimul din vector
+    - cautare secventiala
+    - cautare binara
+    - ordonare prin metoda bulelor
+    - sa se specializeze functia ordonare pentru vectorii de caractere a.i. sa fie
+        sortate in ordine invera alfabetica
+*/
+
+
+#include <iostream>
+#include "vector.h"
+
+
+int main(void){
+    using namespace Vector;
+
+    int vector[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    std::cout << "Max: " << max<int>(10, vector) << std::endl;
+    std::cout << "Min: " << min<int>(10, vector) << std::endl;
+    std::cout << "Search True: " << search<int>(10, vector, 5) << std::endl;
+    std::cout << "Search False: " << search<int>(10, vector, -1) << std::endl;
+    std::cout << "BSearch True: " << bsearch<int>(10, vector, 10) << std::endl;
+    std::cout << "BSearch False: " << bsearch<int>(10, vector, -1) << std::endl;
+
+    char vect[10] {'a', 'z', 'y', 'm', 'c', 'd', 't', 'o', 'g', 'e'};
+    std::cout << "Bubble Sort: ";
+    bubbleSort(10, vect);
+    printVector(10, vect);
+    std::cout << "Reverse Sort: ";
+    reverseSort(10, vect);
+    printVector(10, vect);
+
+    return 0;
+}
---
+++ Year - 2/Semester 1/Advanced Programming Techniques/Lab 7/Ex1/vector.h
@@ -0,0 +1,106 @@
+#pragma once
+#include <iostream>
+
+
+
+namespace Vector{
+    template <typename T>
+    T max(int size, T vector[]){
+        T m = vector[0];
+
+        for(int i = 1; i < size; ++i){
+            if(m < vector[i]){
+                m = vector[i];
+            }
+        }
+
+        return m;
+    }    
+
+    template <typename T>
+    T min(int size, T vector[]){
+        T m = vector[0];
+
+        for(int i = 1; i < size; ++i){
+            if(m > vector[i]){
+                m = vector[i];
+            }
+        }
+
+        return m;
+    }
+
+    template <typename T>
+    bool search(int size, T vector[], T key){
+        for(int i = 0; i < size; ++i){
+            if(key == vector[i]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    template <typename T>
+    bool bsearch(int size, T vector[], T key){
+        int min = 0,
+            max = size-1,
+            i = size / 2;
+
+        while(i > 0 && i < size){
+            if(key == vector[i]){
+                return true;
+            }
+
+            if(key < vector[i]){
+                max = i - 1;
+                i = (min + max) / 2;
+                continue;
+            }
+
+            if(key > vector[i]){
+                min = i + 1;
+                i = (min + max) / 2;
+            }
+        }
+
+        return false;
+    }
+
+    template <typename T>
+    void bubbleSort(int size, T vector[]){
+        for(int i=0; i<size-1; ++i){
+            for(int j=0; j<size-i-1; ++j){
+                if(vector[j] > vector[j+1])
+                {
+                    T aux = vector[j];
+                    vector[j] = vector[j+1];
+                    vector[j+1] = aux;
+                }
+            }
+        }
+    }
+
+    template <typename T>
+    void reverseSort(int size, T vector[]){
+        for(int i=0; i<size-1; ++i){
+            for(int j=0; j<size-i-1; ++j){
+                if(vector[j] < vector[j+1])
+                {
+                    T aux = vector[j];
+                    vector[j] = vector[j+1];
+                    vector[j+1] = aux;
+                }
+            }
+        }
+    }
+
+    template <typename T>
+    void printVector(int size, T vector[]){
+        std::cout << "Vector: ";
+        for(int i = 0; i < size; ++i){
+            std::cout << vector[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
