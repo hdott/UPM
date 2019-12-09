f89abeb8b0fd46a050777d3bfd26451e2dfb1662
hdott <n.horia.c@hotmail.com>
2019-12-09 10:58:24 +0200

    wip ex3

--- Year - 2/Semester 1/Advanced Programming Techniques/Lab 10/Ex3/iterator_bi.h
+++ Year - 2/Semester 1/Advanced Programming Techniques/Lab 10/Ex3/iterator_bi.h
@@ -6,5 +6,9 @@
 template<class T>
 class iterator_bi : public iterator<bidirectional_iterator_tag, T>{
     private:
-        Nod<T> 
+        Nod<T> *nod_curent;
+
+    public:
+        typedef typename T value_type;
+        typedef typename T& reference;
 };
